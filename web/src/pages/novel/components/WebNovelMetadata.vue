6d42ab754b470af4e29d0ee8c48747897549b7ad
FishHawk <whang23@outlook.com>
2026-05-14 02:54:24 +0800

    refactor(web): 抽取小说操作组件

--- web/src/pages/novel/components/WebNovelMetadata.vue
+++ web/src/pages/novel/components/WebNovelMetadata.vue
@@ -1,20 +1,11 @@
 <script lang="ts" setup>
-import { BookOutlined, SyncOutlined, EditNoteOutlined } from '@vicons/material';
-import { CrawlerService } from '@/domain/crawler';
 import type { WebNovelDto } from '@/model/WebNovel';
-import { useWhoamiStore } from '@/stores';
-
-import { doAction } from '@/pages/util';
 
 const props = defineProps<{
   providerId: string;
   novelId: string;
   novel: WebNovelDto;
 }>();
-
-const whoamiStore = useWhoamiStore();
-const { whoami } = storeToRefs(whoamiStore);
-const message = useMessage();
 
 const labels = computed(() => {
   const readableNumber = (num: number | undefined) => {
@@ -49,25 +40,6 @@ const generateSearchUrl = (query: string) => {
   return `/novel?query=${encodeURIComponent(query)}`;
 };
 
-const startReadChapter = computed(() => {
-  const { novel } = props;
-  if (novel.lastReadChapterId !== undefined) {
-    const lastReadChapter = novel.toc.find(
-      (it) => it.chapterId === novel.lastReadChapterId,
-    );
-    if (lastReadChapter !== undefined) {
-      return { chapter: lastReadChapter, type: 'continue' };
-    }
-  }
-
-  const firstChapter = novel.toc.find((it) => it.chapterId !== undefined);
-  if (firstChapter !== undefined) {
-    return { chapter: firstChapter, type: 'first' };
-  }
-
-  return undefined;
-});
-
 const latestChapterCreateAt = computed(() => {
   const { novel } = props;
   const createAtList = novel.toc
@@ -76,18 +48,10 @@ const latestChapterCreateAt = computed(() => {
   if (createAtList.length === 0) return undefined;
   else return Math.max(...createAtList);
 });
-
-const updateNovel = () => {
-  return doAction(
-    CrawlerService.updateWebNovel(props.providerId, props.novelId),
-    '更新小说',
-    message,
-  );
-};
 </script>
 
 <template>
-  <web-novel-title
+  <WebNovelTitle
     :provider-id="providerId"
     :novel-id="novelId"
     :title-jp="novel.titleJp"
@@ -101,42 +65,11 @@ const updateNovel = () => {
     </template>
   </n-p>
 
-  <n-flex>
-    <router-link
-      v-if="startReadChapter !== undefined"
-      :to="`/novel/${providerId}/${novelId}/${startReadChapter.chapter.chapterId}`"
-    >
-      <c-button
-        :label="startReadChapter.type === 'continue' ? '继续阅读' : '开始阅读'"
-        :round="false"
-      />
-    </router-link>
-    <c-button v-else label="开始阅读" disabled />
-
-    <favorite-button
-      v-model:favored="novel.favored"
-      :novel="{ type: 'web', providerId, novelId }"
-    />
-
-    <c-button
-      v-if="whoami.hasNovelAccess"
-      label="更新"
-      :round="false"
-      :icon="SyncOutlined"
-      @action="updateNovel()"
-    />
-
-    <router-link
-      v-if="whoami.hasNovelAccess"
-      :to="`/novel-edit/${providerId}/${novelId}`"
-    >
-      <c-button label="编辑" :round="false" :icon="EditNoteOutlined" />
-    </router-link>
-
-    <router-link v-if="novel.wenkuId" :to="`/wenku/${novel.wenkuId}`">
-      <c-button label="文库" :round="false" :icon="BookOutlined" />
-    </router-link>
-  </n-flex>
+  <WebNovelActions
+    :provider-id="providerId"
+    :novel-id="novelId"
+    :novel="novel"
+  />
 
   <n-p>{{ labels }}</n-p>
 
